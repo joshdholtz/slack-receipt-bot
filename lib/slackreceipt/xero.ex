4d3a671f588889032ce0c07b7181e53312d70d65
Josh Holtz <josh@rokkincat.com>
2016-02-22 23:13:42 -0600

    It works... again... dur

--- lib/slackreceipt/xero.ex
+++ lib/slackreceipt/xero.ex
@@ -1,24 +1,27 @@
 defmodule SlackReceipt.Xero do
-  @consumer {Application.get_env(:slackreceipt, :oauth)[:consumer_key], Application.get_env(:slackreceipt, :oauth)[:private_key_path], :rsa_sha1}
+  @consumer_key Application.get_env(:slackreceipt, :oauth)[:consumer_key]
+  @consumer {@consumer_key, Application.get_env(:slackreceipt, :oauth)[:private_key_path], :rsa_sha1}
 
-  def get(url, params) do
-    #authorization_header = :oauth.sign(
-    #   "GET", url, params, @consumer, Application.get_env(:slackreceipt, :oauth)[:consumer_key], ""
-    #  ) |> Enum.map( fn {k,v} ->
-      #    "#{k}=\"#{v}\""
-      #end) |> Enum.join("=")
+  def get(url, params, headers \\ %{}), do: request(:get, url, params, "", headers)
+  def post(url, params, body \\ "", headers \\ %{}), do: request(:post, url, params, body, headers)
+  def put(url, params, body \\ "", headers \\ %{}), do: request(:put, url, params, body, headers)
+  def delete(url, params, headers \\ %{}), do: request(:delete, url, params, "", headers)
 
+  def request(method, url, params, body \\ %{}, headers \\ %{}) do
+
+    # Creates signed params for oauth
     signed_params = :oauth.sign(
-      "GET", url, params, @consumer, Application.get_env(:slackreceipt, :oauth)[:consumer_key], ""
+      Atom.to_string(method) |> String.upcase, url, params, @consumer, @consumer_key, ""
     )
-   
-    headers = %{
+
+    # Creates headers
+    headers = Dict.merge(headers, %{
       "Authorization": "OAuth #{:oauth.header_params_encode(signed_params)}",
       "Accept": "application/json"
-    }
+    }) |> Enum.reduce([], fn ({k,v}, a) -> 
+      a ++ [{Atom.to_string(k), v}]
+    end)
 
-    uri = :oauth.uri(url, signed_params)
-
-    HTTPoison.get(url, headers)
+    HTTPoison.request(method, url, "", headers)
   end
 end

ba58560c4af1dba24017d8d73d73e1ddd51dbdd5
Josh Holtz <josh@rokkincat.com>
2016-04-09 14:02:21 -0500

    OMG this works

--- lib/slackreceipt/xero.ex
+++ lib/slackreceipt/xero.ex
@@ -7,7 +7,49 @@ defmodule SlackReceipt.Xero do
     request(:get, "https://api.xero.com/api.xro/2.0/Accounts")
   end
 
-  def request(method, url, params \\ [], body \\ %{}, headers \\ %{}, options \\ []) do
+  def files do
+    request(:get, "https://api.xero.com/files.xro/1.0/Files")
+  end
+
+  def post_file(path_to_file, folder_id) do
+    method = :post
+    body = {:multipart, [ {"name", "a_name"}, {:file, path_to_file} ] }
+    headers = %{
+    }
+    options = []
+    params = ["name": "name"]
+    url = "https://api.xero.com/files.xro/1.0/Files"
+
+    #request(:post, "https://api.xero.com/files.xro/1.0/Files/#{path_to_file}", [], body)
+
+    # Creates signed params for oauth
+    signed_params = :oauth.sign(
+      Atom.to_string(method) |> String.upcase, String.to_char_list(url), params, @consumer, @consumer_key, ""
+    )
+
+    default_headers =  %{
+      "Authorization": "OAuth #{:oauth.header_params_encode(signed_params)}",
+      "Accept": "application/json",
+    }
+
+    headers = Map.merge(default_headers, headers)
+
+    HTTPoison.post(url, {:multipart, [{:file, path_to_file}]}, headers, options)
+  end
+
+  def folders do
+    request(:get, "https://api.xero.com/files.xro/1.0/Folders")
+  end
+
+  def create_folder(name) do
+    body = %{name: name} |> Poison.encode!
+    headers = %{
+      "Content-Type": "application/json"
+    }
+    request(:post, "https://api.xero.com/files.xro/1.0/Folders", [], body, headers)
+  end
+
+  def request(method, url, params \\ [], body \\ "", headers \\ %{}, options \\ []) do
     # Creates signed params for oauth
     signed_params = :oauth.sign(
       Atom.to_string(method) |> String.upcase, String.to_char_list(url), params, @consumer, @consumer_key, ""
@@ -19,7 +61,6 @@ defmodule SlackReceipt.Xero do
     }
 
     headers = Map.merge(default_headers, headers)
-
-    HTTPoison.request(method, url, "", headers, options)
+    HTTPoison.request(method, url, body, headers, options)
   end
 end

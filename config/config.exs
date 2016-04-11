2a8e2af1862df34d4f1130bdc313e503ec626317
Josh Holtz <josh@rokkincat.com>
2016-04-11 13:43:43 -0500

    Channel stuff

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,8 @@
 use Mix.Config
 
 config :slackreceipt, :slack,
-  token: System.get_env("SLACK_TOKEN")
+  token: System.get_env("SLACK_TOKEN"),
+  channel_id: System.get_env("SLACK_CHANNEL_ID")
 config :slackreceipt, :oauth,
   consumer_key: System.get_env("XERO_CONSUMER_KEY"),
   private_key_contents: System.get_env("XERO_PRIVATE_KEY_CONTENTS")
@@ -11,3 +12,4 @@ config :slackreceipt, :oauth,
 if File.exists?("config/config.secret.exs") do
   import_config "config.secret.exs"
 end
+
--- lib/slackreceipt/slack.ex
+++ lib/slackreceipt/slack.ex
@@ -2,10 +2,23 @@ defmodule SlackReceipt.Slack do
   use Slacker
 
   @slack_token Application.get_env(:slackreceipt, :slack)[:token]
+  @slack_channel_id Application.get_env(:slackreceipt, :slack)[:channel_id]
 
   def handle_cast({:handle_incoming, "file_shared", msg}, state) do
     Logger.debug "A file was shared or something"
     IO.inspect msg
+
+    #Enum.find(channels, fn(%{"name"=>name}) -> name == "receipts"end)
+
+    channels = msg["file"]["channels"]
+    if Enum.member?(channels, @slack_channel_id) do
+      send_image(msg, state, channels)
+    end
+
+    {:noreply, state}
+  end
+
+  def send_image(msg, state, channels) do
     # Slack info
     url = msg["file"]["url_private_download"]
     user_id = msg["file"]["user"]
@@ -31,15 +44,18 @@ defmodule SlackReceipt.Slack do
     IO.inspect resp
 
     # Report back to channels about the thing
-    channels = msg["file"]["channels"]
     Enum.each(channels, fn(channel) ->
       say self, channel, "Receipt has been uploaded to Xero :)"
     end)
-    {:noreply, state}
   end
 
   def get_user(user_id) do
     url = "https://slack.com/api/users.info?token=#{@slack_token}&user=#{user_id}"
+    HTTPoison.get!(url)
+  end
+
+  def get_channels do
+    url = "https://slack.com/api/channels.list?token=#{@slack_token}"
     HTTPoison.get!(url)
   end
 
